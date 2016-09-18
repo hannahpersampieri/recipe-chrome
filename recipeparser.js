605bebd69b93c8966247ef93d1652b42864788a3
Hannah Voelker <hannahvoelker13@gmail.com>
2016-09-17 22:11:50 -0400

    updated recipeparser

--- recipeparser.js
+++ recipeparser.js
@@ -3,12 +3,42 @@ var info = document.getElementsByTagName("body")[0].innerHTML;
 // We need to get ingredients, recipe yield for our purposes
 var ingredients_div = document.querySelectorAll('[itemprop = "ingredients"]');
 var ingredients = [];
+
+function isUnit(word){
+	var measurement = ["cups", "cup", "teaspoon", "teaspoons","tablespoon", "tablespoons", "ounces", "oz", "pounds", "lb", "pound", "oz"]
+	for(k = 0; k < measurement.length; k++){
+		if(measurement[k] == word){
+			return true;
+		}
+	}
+	return false;
+}
+
 for(i = 0; i < ingredients_div.length; i++){
-	ingredients.push(ingredients_div[i].innerText);
+	var info = {};
+	var inner_text = ingredients_div[i].innerText.split(" ");
+	for(j = 0; j < inner_text.length; j++){
+		if (parseInt(inner_text[j])){
+			if(j > 0 && parseInt(inner_text[j-1])){
+				info["amount"] = info["amount"]+ " "+inner_text[j];
+			}
+			else {
+				info["amount"] = inner_text[j];
+			}
+		}
+		else if (isUnit(inner_text[j])){
+			info["unit"] = inner_text[j];
+		}
+		else {
+			info["food"] = inner_text[j];
+		}
+
+	}
+	ingredients.push(info);
 }
 /* TO - DO
 	- remove measurements
-	- remove , and any details after
+	- remove , and any details after (?)
 	- push to popup
 */
 console.log(ingredients);
