9efe16bec202244d196ac56f1a71355af6885045
Hannah Voelker <hannahvoelker13@gmail.com>
2016-09-19 19:14:49 -0400

    begin attempt at data pushing

--- popup.js
+++ popup.js
@@ -5,11 +5,12 @@ var details = {
 var callback = function(results) {
 	var ingredients = results[0];
 	var list = document.getElementById("ingredientsList");
-	document.write("<h1> Recipe: Add this meal </h1>");
+	document.write("<h1 style=\"color:#3399ff; font-family:\"Helvetica\"> Recipe: Add this meal </h1>");
 	for(i = 0; i < ingredients.length; i++){
 		var itemstr = ingredients[i]["amount"] + " " + ingredients[i]["unit"]+" "+ingredients[i]["food"];
-		document.write("<li>"+itemstr+"</id>");
+		document.write("<input type=\"checkbox\"/><label>"+itemstr+"</label><br>");
 	}
+	document.write("<input type= \"submit\" value = \"submit\">")
 };
 chrome.tabs.executeScript(details, callback);
 
